8fa0e8dc1fd06a73767f4fb1433b5d725a5ab639
Nick Cooper <cooper.b.nicholas@gmail.com>
2020-09-05 11:35:06 -0500

    add errors

---
+++ hautomate/errors.py
@@ -0,0 +1,12 @@
+
+
+class HautoException(Exception):
+    """
+    Base exception for the HAutomate library.
+    """
+
+
+class CheckError(Exception):
+    """
+    Raise when a Check fails.
+    """
