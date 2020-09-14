b6574caf217b14d5fc1f63c53f464547de3fffe4
Nick Cooper <cooper.b.nicholas@gmail.com>
2020-09-13 19:23:56 -0500

    add api tests

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -1,3 +1,5 @@
+import time
+
 from ward import test
 
 from hautomate import HAutomate
@@ -5,10 +7,41 @@ from hautomate import HAutomate
 from tests.fixtures import cfg_hauto
 
 
-@test('APIRegistry')
-async def _(cfg=cfg_hauto):
-    """ async because we call loop.create_task upon AppRegistry.load(app) """
+@test('APIRegistry autosetup runs on builtin apis')
+def _(cfg=cfg_hauto):
+    # overwrite any existing api configs
+    cfg.api_configs = {}
+
     hauto = HAutomate(cfg)
     hauto.apis._load_all_apis(None)
+    hauto.apis['trigger']
+    hauto.apis['moment']
 
-    assert 1 == 2
+
+# @test('Moment API allows time travel: rate={rate}, epoch={epoch}')
+# async def _(cfg=cfg_hauto, rate=each(), epoch=each(), end=each()):
+#     # overwrite any existing api configs
+#     cfg.api_configs = {
+#         'moment': {
+#             'rate': rate,
+#             'epoch': epoch
+#         }
+#     }
+
+#     hauto = HAutomate(cfg)
+#     # need to start
+#     hauto.start()
+#     hauto.apis._load_all_apis(None)
+
+#     real_beg = time.perf_counter()
+#     virt_beg = hauto.now
+#     # sleep 1.0s
+#     real_end = time.perf_counter()
+#     virt_end = hauto.now
+
+#     real_elapsed = time.perf_counter() - real_beg
+#     virt_elapsed = hauto.now - virt_beg
+
+#     assert real_elapsed == 1.0
+#     assert virt_elapsed == 
+#     assert virt_end == end
