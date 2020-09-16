34efcfbc6b0ca544957fbbcb61f3e421fdcf246c
Nick Cooper <cooper.b.nicholas@gmail.com>
2020-09-16 15:36:04 -0500

    adding builtin api basics

--- hautomate/apis/moment/moment.py
+++ hautomate/apis/moment/moment.py
@@ -1,13 +1,20 @@
+import asyncio
+import logging
 import time
 
 import pendulum
 
+from hautomate.apis.moment.events import EVT_TIME_UPDATE, EVT_TIME_SLIPPAGE
+from hautomate.context import Context
 from hautomate.api import API
+
+
+_log = logging.getLogger(__name__)
 
 
 class Moment(API):
     """
-
+    API for working with absolute and relative dates and times.
 
     Parameters
     ----------
@@ -44,3 +51,38 @@ class Moment(API):
         """
         elapsed = time.perf_counter() - self._monotonic_epoch
         return self.epoch.add(seconds=elapsed * self.speed)
+
+    # Listeners and Internal Methods
+
+    async def on_ready(self, ctx: Context):
+        """
+        Called once HAutomate is ready to begin processing events.
+        """
+        asyncio.create_task(self._tick())
+
+    async def _tick(self):
+        """
+        Internal heartbeat to determine the health of HAutomate.
+        """
+        while not self.hauto.is_ready:
+            asyncio.create_task(self.fire(EVT_TIME_UPDATE))
+            before = self.hauto.loop.time()
+            await asyncio.sleep(self.resolution)
+            after = self.hauto.loop.time()
+            lag = (after - before) - 1
+
+            if lag >= self.resolution:
+                _log.warning(f'lag of {lag :.6f}s, {round(lag * 1000)}ms')
+                coro = self.fire(EVT_TIME_SLIPPAGE, lag=lag)
+                asyncio.create_task(coro)
+
+    # Public Methods
+
+    def scale_to_realtime(self, seconds: float) -> float:
+        """
+        Convert the input time, scaled by the time factor in HAutomate.
+
+        This is mainly used for interacting with time based systems
+        outside of HAutomate.
+        """
+        return seconds / self.speed
--- hautomate/apis/trigger/trigger.py
+++ hautomate/apis/trigger/trigger.py
@@ -1,3 +1,8 @@
+import collections
+import asyncio
+
+from hautomate.context import Context
+from hautomate.events import EVT_ANY
 from hautomate.api import API
 
 
@@ -5,3 +10,61 @@ class Trigger(API):
     """
 
     """
+    def __init__(self, hauto):
+        super().__init__(hauto)
+        self._event_waiters = collections.defaultdict(lambda: asyncio.Event())
+
+    #
+
+    async def on_start(self, ctx: Context, *args, **kwargs):
+        """
+        Called once HAutomate is ready to begin processing events.
+        """
+        # self.on(EVT_ANY, method=self.on_almost_any_event)
+        from hautomate.intent import Intent
+        from hautomate.events import (
+            _EVT_INIT, EVT_APP_LOAD, EVT_APP_UNLOAD,
+            EVT_INTENT_SUBSCRIBE, EVT_INTENT_START, EVT_INTENT_END
+        )
+        from hautomate.check import Check
+
+        _META_EVENTS = (
+            _EVT_INIT, EVT_APP_LOAD, EVT_APP_UNLOAD,
+            EVT_INTENT_SUBSCRIBE, EVT_INTENT_START, EVT_INTENT_END
+        )
+
+        check = Check(lambda ctx: ctx.event not in _META_EVENTS)
+        intent = Intent(EVT_ANY, self.on_almost_any_event, checks=[check])
+        self.hauto.bus.subscribe(EVT_ANY, intent)
+
+    async def on_almost_any_event(self, ctx: Context, *args, **kwargs):
+        """
+        Called on every event except TIME_UPDATE.
+
+        This first time an event is fired, an asyncio.Event is created. The
+        event grabbed is set and cleared immediately, triggering all coroutines
+        who wait on the event to be awakened. The event is then cleared so that
+        new waiters may block until the next event fire.
+        """
+        evt = self._event_waiters[ctx.event]
+        evt.set()
+        evt.clear()
+
+    # PUBLIC METHODS
+
+    async def wait_for(self, event_name: str):
+        """
+        Block until the next time <event_name> is seen.
+
+        This method can be used to await any incoming event except TIME_UPDATE.
+        It is particularly handy when waiting for an outside (of HAutomate)
+        event to be pushed into the platform.
+        """
+        evt = event_name.upper()
+
+        # TODO
+        #   optionally, we can extend this with a timeout param & asyncio.wait_for()
+        #
+        # evt = self._event_waiters[event_name]
+        # await asyncio.wait_for(evt.wait(), timeout)
+        await self._event_waiters[evt].wait()
