0af4a321cee8f4d62aefc0108d6ffbb90d62e121
Nick Cooper <cooper.b.nicholas@gmail.com>
2020-09-23 23:09:43 -0500

    adding trigger's api methods

--- hautomate/apis/trigger/trigger.py
+++ hautomate/apis/trigger/trigger.py
@@ -56,15 +56,143 @@ class Trigger(API):
 
     # Intents
 
-    # @api_method
-    # def on(self, event_name: str, *, method=None, **intent_kwargs) -> Intent:
-    #     """
+    @api_method
+    def on(self, event_name: str, *, fn: Callable, **intent_kwargs) -> Intent:
+        """
+        Listen for a specifc event.
 
-    #     trigger.on(some_event, method=lambda ctx: None)
+        Can be used as an App function decorator. If used inline, this
+        method expects a keyword argument 'fn', the intended method to
+        call upon meeting the criteria. All other keyword arguments are
+        passed into the Intent.
 
-    #     @trigger.on(some_event)
-    #     """
-    #     if method is None:
-    #         pass
+        Parameters
+        ----------
+        event_name : str
+            event to listen for
 
-    #     return Intent()
+        Returns
+        -------
+        intent : Intent
+        """
+        return Intent(event_name, fn=fn, **intent_kwargs)
+
+    @api_method
+    def any(self, *event_names: Tuple[str], fn: Callable, **intent_kwargs) -> Intent:
+        """
+        Listen for any event in <event_names>.
+
+        Can be used as an App function decorator. If used inline, this
+        method expects a keyword argument 'fn', the intended method to
+        call upon meeting the criteria. All other keyword arguments are
+        passed into the Intent.
+
+        Parameters
+        ----------
+        *event_names : tuple[str]
+            names of events to listen for
+
+        Returns
+        -------
+        intent : Intent
+        """
+        evts = '|'.join(map(str.upper, event_names))
+        return self.re_match(evts, fn=fn, subscribe=False, **intent_kwargs)
+
+    @api_method
+    def startswith(self, part: str, *, fn: Callable, **intent_kwargs) -> Intent:
+        """
+        Listen to events that begin with <part>.
+
+        Can be used as an App function decorator. If used inline, this
+        method expects a keyword argument 'fn', the intended method to
+        call upon meeting the criteria. All other keyword arguments are
+        passed into the Intent.
+
+        Parameters
+        ----------
+        part : str
+            partial match of the events to listen for
+
+        Returns
+        -------
+        intent : Intent
+        """
+        return self.re_match(fr'^{part.upper()}.*', fn=fn, **intent_kwargs)
+
+    @api_method
+    def endswith(self, part: str, *, fn: Callable, **intent_kwargs) -> Intent:
+        """
+        Listen to events that end with <part>.
+
+        Can be used as an App function decorator. If used inline, this
+        method expects a keyword argument 'fn', the intended method to
+        call upon meeting the criteria. All other keyword arguments are
+        passed into the Intent.
+
+        Attributes
+        ----------
+        part : str
+            partial match of the events to listen for
+
+        Returns
+        -------
+        intent : Intent
+        """
+        return self.re_match(fr'.*{part.upper()}$', fn=fn, **intent_kwargs)
+
+    @api_method
+    def contains(self, mid: str, *, fn: Callable, **intent_kwargs) -> Intent:
+        """
+        Listen to events that have <mid> within.
+
+        Can be used as an App function decorator. If used inline, this
+        method expects a keyword argument 'fn', the intended method to
+        call upon meeting the criteria. All other keyword arguments are
+        passed into the Intent.
+
+        Attributes
+        ----------
+        mid : str
+            partial match of the events to listen for
+
+        Returns
+        -------
+        intent : Intent
+        """
+        return self.re_match(fr'.*{mid.upper()}.*', fn=fn, **intent_kwargs)
+
+    @api_method
+    def re_match(
+        self,
+        pattern: str,
+        *,
+        fn: Callable,
+        **intent_kwargs
+    ) -> Intent:
+        """
+        Listen for any event which matches a regex.
+
+        Can be used as an App function decorator. If used inline, this
+        method expects a keyword argument 'fn', the intended method to
+        call upon meeting the criteria. All other keyword arguments are
+        passed into the Intent.
+
+        Parameters
+        ----------
+        pattern : str
+            regex pattern to re.fullmatch against
+
+        Returns
+        -------
+        intent : Intent
+        """
+        try:
+            intent_kwargs['checks']
+        except KeyError:
+            intent_kwargs['checks'] = []
+
+        pattern = re.compile(fr'{pattern}', flags=re.IGNORECASE)
+        check = Check(lambda ctx: pattern.fullmatch(ctx.event) is not None)
+        intent_kwargs['checks'].append(check)
+        return Intent(EVT_ANY, fn=fn, **intent_kwargs)
