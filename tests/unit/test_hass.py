cd01b78a79788b38c78080ff0eb39abd33388d5f
Nick Cooper <cooper.b.nicholas@gmail.com>
2020-09-26 16:52:51 -0500

    tests for homeassistant settings validation

---
+++ tests/unit/test_hass.py
@@ -0,0 +1,39 @@
+from ward import test, each, raises
+
+from homeassistant.core import HomeAssistant
+from hautomate.settings import HautoConfig
+from hautomate import Hautomate
+import pydantic
+
+from tests.fixtures import cfg_data_hauto
+
+
+@test('HomeAssistantConfig validates for {feed}', tags=['unit'])
+async def _(
+    cfg_data=cfg_data_hauto,
+    feed=each('custom_component', 'custom_component', 'websocket'),
+    hass_cls=each(HomeAssistant, None, None)
+):
+    if hass_cls is not None:
+        hass = hass_cls()
+    else:
+        hass = None
+
+    # overwrite any existing api configs
+    data = cfg_data.copy()
+
+    data['api_configs'] = {
+        'homeassistant': {
+            'feed': feed,
+            'hass_interface': hass
+        }
+    }
+
+    if feed == 'custom_component' and hass is None:
+        with raises(pydantic.ValidationError):
+            cfg = HautoConfig(**data)
+    else:
+        cfg = HautoConfig(**data)
+        hauto = Hautomate(cfg)
+        assert hauto.is_running is True
+        assert hauto.is_ready is False
