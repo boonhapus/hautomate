d3582b710756874be2fafc266f69e4090c6a6966
Nick Cooper <cooper.b.nicholas@gmail.com>
2020-09-26 23:48:21 -0500

    or maybe i'm just retarded 🙃

--- hautomate/apis/__init__.py
+++ hautomate/apis/__init__.py
@@ -1,3 +1,3 @@
-from hautomate.apis.homeassistant.homeassistant import HomeAssistant as hass
+from hautomate.apis.homeassistant.homeassistant import HomeAssistant as homeassistant
 from hautomate.apis.trigger.trigger import Trigger as trigger
 from hautomate.apis.moment.moment import Moment as moment
