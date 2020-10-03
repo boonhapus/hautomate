boonhapus/hautomate

commit 4bf213e832f1693c3e36af38e144cb63323b1561

break race condition in intent runner

hautomate/intent.py
@@ -129,9 +129,11 @@ class Intent(Asyncable):
         """
         self.runs += 1
 
-        # TODO needed here to beat 2 copies of the same intent fired simultaneously?
-        # if self.limit - self.runs == -1:
-        #     return
+        # break race condition where more than 1 copy of an Intent can
+        # qualify all check during the same iteration of the event loop,
+        # but only one should actually run.
+        if self.limit - self.runs < 0:
+            return
 
         r = await super().__call__(ctx, *a, loop=ctx.hauto.loop, **kw)
         self.last_ran = ctx.hauto.now

commit fe656a47216b95fb897cd36f6909e11c4dcf1f28

🐛 fix bug introduced in 4bf213e regarding fixing a race condition 🙃

hautomate/intent.py
@@ -132,7 +132,7 @@ class Intent(Asyncable):
         # break race condition where more than 1 copy of an Intent can
         # qualify all check during the same iteration of the event loop,
         # but only one should actually run.
-        if self.limit - self.runs < 0:
+        if self.runs > self.limit > 0:
             return
 
         r = await super().__call__(ctx, *a, loop=ctx.hauto.loop, **kw)