9ae62bdf524d4adbc3049668fdd2662e5d45a6de
Nick Cooper <cooper.b.nicholas@gmail.com>
2020-10-14 23:39:11 -0500

    add input_number

--- hautomate/apis/homeassistant/helpers.py
+++ hautomate/apis/homeassistant/helpers.py
@@ -1,4 +1,5 @@
 from homeassistant.components.input_boolean import InputBoolean, InputBooleanStorageCollection
+from homeassistant.components.input_number import InputNumber, InputNumberStorageCollection
 from homeassistant.components.input_text import InputText, InputTextStorageCollection
 from homeassistant.helpers.entity import Entity
 
@@ -8,7 +9,17 @@ def create_input_text(name, **data) -> Entity:
     """
     data['name'] = name
     cfg = InputTextStorageCollection.CREATE_SCHEMA(data)
+    cfg['id'] = name
     return InputText.from_yaml(cfg)
+
+
+def create_input_number(name, **data) -> Entity:
+    """
+    """
+    data['name'] = name
+    cfg = InputNumberStorageCollection.CREATE_SCHEMA(data)
+    cfg['id'] = name
+    return InputNumber.from_yaml(cfg)
 
 
 def create_input_boolean(name, **data) -> Entity:
